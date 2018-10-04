f10fb8a4387df00086acbb7719e0b4ae400b389f
Herbert Treis Neto <htnetooo@gmail.com>
2018-10-04 19:43:45 -0300

    Identation to match .editorconfig [closes #18]

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,18 +1,20 @@
 // TypeScript Version: 3.1.1
 
-type formatOption = { [key: string]: string };
+type formatOption = {
+  [key: string]: string
+};
 
 interface TimeCallback {
-    add(value: string | number, unit: string): TimeCallback;
-    subtract(value: string | number, unit: string): TimeCallback;
-    toNumber(value: string | number, unit: string): TimeCallback;
-    format(options?: formatOption): void;
+  add(value: string | number, unit: string): TimeCallback;
+  subtract(value: string | number, unit: string): TimeCallback;
+  toNumber(value: string | number, unit: string): TimeCallback;
+  format(options?: formatOption): void;
 }
 
 declare namespace time {
-    export const HOURS: string;
-    export const MINUTES: string;
-    export const SECONDS: string;
+  export const HOURS: string;
+  export const MINUTES: string;
+  export const SECONDS: string;
 }
 
 declare function time(value: string | number, unit?: string): TimeCallback;
